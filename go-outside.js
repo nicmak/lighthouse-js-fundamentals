845fc6cba9c2f1990184e9384c83e0f841f0d89e
Nicholas Mak <nicholas123mak@gmail.com>
2016-10-19 21:38:31 +0000

    Adds conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,12 @@
+var temperature = 12
+
+if (temperature < 0) {
+  console.log("Get a scarf its really cold")
+} else if (temperature < 15) {
+  console.log("short sleeves aren't good.")
+} else {
+  console.log("Short sleeves are fine")
+}
+
+console.log("Now you're ready to go outside")
+
