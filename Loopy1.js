1fc9d557061c5f67ea25e54ce612820d87b7f845
Nicholas Mak <nicholas123mak@gmail.com>
2016-10-20 17:00:06 +0000

    New Files again

---
+++ Loopy1.js
@@ -0,0 +1,13 @@
+var number = 100;
+while (number <= 200){
+  if ((number % 3 === 0) && (number % 4 === 0)){
+    console.log("LoopyLighthouse");
+  }else if (number % 3 === 0){
+    console.log("Loopy");     
+  }else if (number % 4 === 0){
+    console.log("Lighthouse");
+  }else{
+    console.log(number);  
+  }
+  number = number + 1; 
+}  	
---
+++ ingredients.js
@@ -0,0 +1,12 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+var counter=0
+while (counter< 8 ); {
+  console.log(ingredients[counter]);
+  counter++;
+}
+
+// Write a for loop that prints out the contents of ingredients:
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
