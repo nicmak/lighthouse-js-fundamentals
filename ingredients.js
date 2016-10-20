27c0df0f3918cd08b7baabb69e4b39ac8733472f
Nicholas Mak <nicholas123mak@gmail.com>
2016-10-20 17:42:29 +0000

    New thursday

--- ingredients.js
+++ ingredients.js
@@ -1,12 +1,19 @@
 var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
 
 // Write a while loop that prints out the contents of ingredients:
-var counter=0
-while (counter< 8 ); {
+var counter=0;
+while (counter < 8 ){
   console.log(ingredients[counter]);
   counter++;
 }
 
 // Write a for loop that prints out the contents of ingredients:
+for (var counter2 = 0; counter2 < 8; counter2++) {
+  console.log(ingredients[counter2])
+}
 
 // Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+for (var counter3 = 7; counter3 >= 0; counter3--) {
+  console.log(ingredients[counter3]);
+}  
--- school.js
+++ school.js
@@ -1,12 +1,11 @@
-function whichSchool(age) {
-  if (age < 13) {
-  	console.log("Elementary School");
-  } else if (age < 18) {
-  	console.log("Secondary School");
-  }	else {
-  	console.log("Lighthouse Labs");
-  	}
-  
+function whichSchool(age){
+  if (age < 13){
+    return ("Elementary School");
+  }else if(age >= 13 && age <= 18){
+    return("Secondary School");
+  }else{
+    return ("Lighthouse Labs");
+  }
 }
 
 console.log("I am 35. Which school should I go to?");
